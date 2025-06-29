04e1f874514435fc672e4a8b21118355cf0253ad
balo <balo@alexandria.pc>
2025-06-29 11:32:31 +0100

    Hot fix

--- app/auth/callback/route.ts
+++ app/auth/callback/route.ts
@@ -1,3 +1,5 @@
+export const dynamic = 'force-dynamic';
+
 import { createClient } from '@/lib/supabase/server';
 import { cookies } from 'next/headers';
 import { NextRequest, NextResponse } from 'next/server';
